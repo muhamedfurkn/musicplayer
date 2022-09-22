343f32ce9bc211a3a39f4721d3f054f130d8c79f
MUHAMMED FURKAN ERDOĞAN <110226628+muhamedfurkn@users.noreply.github.com>
2022-09-23 00:12:47 +0300

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,40 @@
+let allMusic = [
+  {
+    name: "Suçlarımdan Biri",
+    artist: "Güneş",
+    img: "music-1",
+    src: "music-1"
+},
+{
+  name: "Beatcoin",
+  artist: "Ceza",
+  img: "music-2",
+  src: "music-2"
+},
+{
+  name: "Dip",
+  artist: "madrigal",
+  img: "music-3",
+  src: "music-3"
+},
+{
+  name: " Another Love",
+  artist: "Tom Odell ",
+  img: "music-4",
+  src: "music-4"
+},
+{
+  name: "Öz Eleştiri",
+  artist: "Uğur Işılak",
+  img: "music-5",
+  src: "music-5"
+},
+    
+    // like this paste it and remember to give comma after ending of this bracket }
+    // {
+    //   name: "Here is the music name",
+    //   artist: "Here is the artist name",
+    //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+    //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+    // }
+  ];
